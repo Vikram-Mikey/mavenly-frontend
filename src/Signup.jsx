b544990bd7145005dae75f13b744d652905c178a
Vikram-Mikey <r.vikram.s321@gmail.com>
2025-07-26 12:32:11 +0530

    19 commit

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import API_BASE_URL from './config';
-import './styles/auth.css';
 
 export default function Signup() {
   const navigate = useNavigate();

b35ec4e1ad353331b51faa6faf088236d0c464e9
vikram <r.vikram.s321@gmail.com>
2025-08-29 17:02:37 +0530

    Update WhatsAppButton.jsx
    changed the phone number

--- src/WhatsAppButton.jsx
+++ src/WhatsAppButton.jsx
@@ -8,7 +8,7 @@ function WhatsAppButton() {
         src="/WhatsApp.svg.png"
         alt="WhatsApp Chat"
         style={{ width: 60, height: 60, cursor: 'pointer', borderRadius: '50%', boxShadow: '0 2px 8px rgba(0,0,0,0.18)' }}
-        onClick={() => window.open('https://wa.me/916380806142', '_blank')}
+        onClick={() => window.open('https://wa.me/916383687601', '_blank')}
       />
     </div>
   );
